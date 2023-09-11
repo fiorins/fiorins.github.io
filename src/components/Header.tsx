747e7c19126541983f8737396d38e4633b1d85ba
Davide <davidefiorini96@gmail.com>
2023-09-11 17:11:47 +0200

    add home link

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,6 +8,7 @@ import {
   Tooltip,
   useBreakpointValue,
 } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
 import { VerifiedIcon } from "../components/icons/VerifiedIcon";
 import ColorModeSwitcher from "../components/shared/ColorModeSwitcher";
 
@@ -16,12 +17,17 @@ export const Header = () => {
 
   return (
     <Flex alignItems="center" justifyContent="space-between" w="full">
-      <HStack spacing={2}>
-        <Heading fontFamily="heading" size="lg">
-          @DAVIDEFIORINI
-        </Heading>
-        <VerifiedIcon h={{ base: "7vmin", md: "5vmin" }} pointerEvents="none" />
-      </HStack>
+      <Link to="/" relative="path">
+        <HStack spacing={2}>
+          <Heading fontFamily="heading" size="lg">
+            @DAVIDEFIORINI
+          </Heading>
+          <VerifiedIcon
+            h={{ base: "7vmin", md: "5vmin" }}
+            pointerEvents="none"
+          />
+        </HStack>
+      </Link>
       <Spacer />
 
       <Tooltip
