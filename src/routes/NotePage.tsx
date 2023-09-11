28bc60d1c5eb875149f7b5aa8fb8320dbcb4df7a
Davide <davidefiorini96@gmail.com>
2023-09-11 22:44:32 +0200

    test loaders

--- src/routes/NotePage.tsx
+++ src/routes/NotePage.tsx
@@ -1,62 +1,98 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @typescript-eslint/no-unused-vars */
-// import { useEffect } from "react";
-import { useParams } from "react-router-dom";
-import { MDXProvider } from "@mdx-js/react";
-import TestNote1 from "../assets/notes/test-note-1.mdx";
-import TestNote2 from "../assets/notes/test-note-2.mdx";
-import { VStack } from "@chakra-ui/react";
-import MDXRenderer from "../components/MdxRenderer";
-import mdxFiles from "../assets/notes/test-note-2.mdx";
+import { useEffect } from "react";
+import { useLoaderData, Form, redirect, useParams } from "react-router-dom";
+// import TestNote1, { frontmatter } from "../assets/notes/test-note-1.mdx";
+import NoteLayout from "../components/NoteLayout";
+import AllNotes from "../assets/notes";
 
-const notes = {
-  note1: TestNote1,
-  note2: TestNote2,
-  // ...
-  // You can add other notes here
-};
+// const notes1 = [
+//   { "test-note-1": AllNotes.TestNote1 },
+//   { "test-note-2": AllNotes.TestNote1 },
+// ];
 
-// type slugProp = {
-//   title?: string;
+// const notes2 = {
+//   "test-note-1": {
+//     frontmatter: "First Blog Post",
+//     content: TestNote1,
+//   },
+//   "test-note-2": {
+//     frontmatter: "Second Blog Post",
+//     content: TestNote1,
+//   },
 // };
 
+export async function loader({ params }: any) {
+  console.log("params: ", params);
+  // const note = await getNote(params.title);
+  const note: string = params.title;
+  console.log("note1: ", note);
+  if (!note) throw new Response("", { status: 404 });
+  return { note };
+}
+
+// export const loader2: any = async ({ params }: any): any => {
+//   //const note: string = params.title;
+//   //console.log("note1: ", note);
+//   //if (!note) throw new Response("", { status: 404 });
+//   return "";
+// };
+
+// export const loader3 =
+//   ({ params }: any) =>
+//   async (slug: string): Promise<{ data: Article }> => {
+//     const query = articleQuery(slug);
+
+//     return await queryClient.ensureQueryData(query);
+//   };
+
+// export const loader = ({ params }: any) => {
+//   return async () => {
+//     const note = params.title;
+//     console.log("note1: ", note);
+//     if (!note) throw new Response("", { status: 404 });
+//     return { note };
+//   };
+// };
+
+// export const loader = ({ params }: any) =>
+//   async () => {
+//     // const note = await getNote(params.title);
+//     const note = params.title;
+//     console.log("note1: ", note);
+//     if (!note) throw new Response("", { status: 404 });
+//     return { note };
+//   };
+
 export default function NotePage() {
-  // //const { title } = useParams<string>();
-  // //const { title } = useParams<"title">();
-  // const { title } = useParams<{ title: string | undefined }>();
-  // //const { title } = useParams();
-  // console.log("title: ", title);
+  const { note } = useLoaderData() as { note: string };
+  console.log("note2: ", note);
+  //const { title } = useParams<{ title: string | undefined }>();
+  //const { title } = useParams<string>();
+  //const { title } = useParams();
 
-  // const NoteComponent: string = notes[title];
-  // //const NoteComponent: string = title ? notes[title] : "got error";
-  // // let NoteComponent = "";
-  // // NoteComponent = title ?? notes[title];
+  // const aboutContent = aboutData.find((item) => item.slug === slug);
 
-  const { title } = useParams();
-  const mdxNote = BlogPosts[slug];
-
-  const mdxFile = mdxFiles.find((file) => file.title === title);
-
-  if (!mdxFile) {
-    return <div>MDX file not found</div>;
-  }
+  console.log("AllNotes: ", AllNotes);
+  //const componentFrontmatter = AllNotes[note];
+  const selectedNote: any = AllNotes.find(
+    (item) => item.frontmatter.slug === note
+  );
+  //const componentContent = AllNotes[note];
+  console.log("selectedNote: ", selectedNote);
 
   return (
-    <VStack
-      minH="100vh"
-      w={{ base: "85%", md: "65%" }}
-      py={8}
-      //gap={{ base: 14, md: 12 }}
-      //spacing={12}
-      //alignItems="center"
-    >
-      {/* <div>Welcome to {title}</div>
-      <div>start xxx start</div>
-      <NoteComponent />
-      <div>middle xxx middle</div>
-      <NoteComponent components={components} />
-      <div>end xxx end</div> */}
-      <MDXRenderer mdxContent={mdxFile.content} />;
-    </VStack>
+    <div>
+      {selectedNote.frontmatter.title}
+      <br />
+      {/* <AllNotes.TestNote1 /> */}
+      <br />
+      Note1
+      <br />
+      {/* <NoteLayout
+        frontmatter={AllNotes.frontmatter}
+        Content={DynamicComponent}
+      /> */}
+    </div>
   );
 }
