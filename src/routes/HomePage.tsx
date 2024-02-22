69a85e832273ebfdbbb78ac292091dba7f8d1a38
Davide <davidefiorini96@gmail.com>
2024-02-22 15:47:35 +0100

    remove pjs

--- src/routes/HomePage.tsx
+++ src/routes/HomePage.tsx
@@ -1,6 +1,6 @@
 import { Welcome } from "../components/home-sections/Welcome";
 import { Spaces } from "../components/home-sections/Spaces";
-import { Projects } from "../components/home-sections/Projects";
+// import { Projects } from "../components/home-sections/Projects";
 // import { Notes } from "../components/home-sections/Notes";
 
 export default function HomePage() {
@@ -8,7 +8,7 @@ export default function HomePage() {
     <>
       <Welcome />
       <Spaces />
-      <Projects />
+      {/* <Projects /> */}
       {/* <Notes /> */}
     </>
   );
